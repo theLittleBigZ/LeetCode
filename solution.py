14dd9cd217e9f70ab7a80e332b17b893b3b6e3ba
Zee Badr <zeeshan.badr@mail.utoronto.ca>
2021-10-07 11:56:29 -0400

    Attempt 2
    55 / 55 test cases passed.
    Runtime: 1208 ms (runtime beats 31.86 % of python3 submissions)
    Memory Usage: 14.9 MB (memory usage beats 80.83 % of python3 submissions)

--- solution.py
+++ solution.py
@@ -1,6 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for x in range(len(nums)):
-            for y in range(len(nums)):
-                if nums[x] + nums[y] == target and not x == y:
-                    return [x,y]
+            find = target - nums[x]
+            holder = nums.copy()
+            holder[x] = "a"
+            if find in holder:
+                return [x, holder.index(find)]
