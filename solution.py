cf928c314d1cb234b4dd8cc2159ce6c2c112931c
Zee Badr <zeeshan.badr@mail.utoronto.ca>
2021-10-08 15:18:07 -0400

    Attempt 4
    11510 / 11510 test cases passed.
    Runtime: 64 ms (runtime beats 64.01 % of python3 submissions)
    Memory Usage: 14.2 MB (memory usage beats 48.12 % of python3 submissions)

--- solution.py
+++ solution.py
@@ -1,7 +1,4 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         num = str(x)
-        for l in range(int(math.ceil(len(num)/2))):
-            if not num[l] == num[-(l+1)]:
-                return False
-        return True
+        return num[::] == num[::-1]
