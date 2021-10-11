6fef062057f1143924990c96ddf6387be2478c4c
Zee Badr <zeeshan.badr@mail.utoronto.ca>
2021-10-11 16:28:36 -0400

    Attempt 2
    91 / 91 test cases passed.
    Runtime: 112 ms (runtime beats 25.28 % of python3 submissions)
    Memory Usage: 14.5 MB (runtime beats 25.28 % of python3 submissions)

--- solution.py
+++ solution.py
@@ -1,6 +1,3 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        l = len(nums)
-        for n in range(l):
-            nums.append(nums[n])
-        return nums
+        return nums * 2
