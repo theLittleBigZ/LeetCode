a57f6808081a4b8f32aa11c9c16fdfdf7bfafc1b
Zee Badr <zeeshan.badr@mail.utoronto.ca>
2021-10-27 19:48:52 -0400

    Attempt 2
    87 / 87 test cases passed.
    Runtime: 32 ms (Runtime: 32 ms, faster than 83.21% of Python3 online submissions)
    Memory Usage: 14.2 MB (Memory Usage: 14.2 MB, less than 47.05% of Python3 online submissions)

--- solution.py
+++ solution.py
@@ -1,21 +1,8 @@
+import math
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        colors = {
-            '0':0,
-            '1':0,
-            '2':0,
-        }
-        for n in nums:
-            if n == 0:
-                colors['0'] += 1
-            elif n == 1:
-                colors['1'] += 1
-            else:
-                colors['2'] += 1
-        ret = ([0]*colors['0']) + [1]*colors['1'] + [2]*colors['2']
-        nums.clear()
-        for n in ret:
-            nums.append(n)
+        nums.sort()
