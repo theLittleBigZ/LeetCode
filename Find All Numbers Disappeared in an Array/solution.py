571b33bb225ad04a0e798675d33271f1a34e789c
Zee Badr (the403) <zeeshan.badr@mail.utoronto.ca>
2021-11-18 16:10:07 -0500

    Attempt 2
    33 / 33 test cases passed.
    Runtime: 356 ms (runtime beats 64.92 % of python3 submissions)
    Memory Usage: 25.2 MB (memory usage beats 15.72 % of python3 submissions)

--- Find All Numbers Disappeared in an Array/solution.py
+++ Find All Numbers Disappeared in an Array/solution.py
@@ -1,5 +1,4 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        complete = range(1, len(nums)+1)
         nums.sort()
-        return list(set(complete) - set(nums))
+        return list(set(range(1, len(nums)+1)) - set(nums))
