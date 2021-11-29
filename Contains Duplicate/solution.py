fc82fb93c00a84e6069ad422798e4f7b18d791a8
Zee Badr (the403) <zeeshan.badr@mail.utoronto.ca>
2021-11-28 22:25:56 -0500

    Attempt 2
    20 / 20 test cases passed.
    Runtime: 120 ms (runtime beats 68.70 % of python3 submissions)
    Memory Usage: 21.5 MB (memory usage beats 19.84 % of python3 submissions)

--- Contains Duplicate/solution.py
+++ Contains Duplicate/solution.py
@@ -1,12 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        counter = {}
+        counter = collections.Counter(nums)
         
-        for num in nums:
-            if num not in counter:
-                counter[num] = 0
-            counter[num] += 1
-        print(counter, max(counter, key=counter.get))
-        if counter[max(counter, key=counter.get)] >= 2:
-            return True
+        for key,value in counter.items():
+            if value >= 2:
+                return True
         return False
