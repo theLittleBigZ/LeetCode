93773eeae98835d67db5c2eab1b1b6d0575c2367
Zee Badr (the403) <zeeshan.badr@mail.utoronto.ca>
2022-08-22 12:16:52 -0400

    Attempt 3
    1061 / 1061 test cases passed.
    Runtime: 42 ms (runtime beats 74.15 % of python3 submissions)
    Memory Usage: 13.9 MB (memory usage beats 54.38 % of python3 submission)

--- Power of Four/Solution.py
+++ Power of Four/Solution.py
@@ -2,4 +2,4 @@ class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n <= 0:
             return False
-        return log(n, 4).is_integer()
+        return log(n, 4) - int(log(n, 4)) == 0
